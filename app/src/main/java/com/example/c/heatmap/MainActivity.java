23bc6afafcf996e2e9794d7d7c757d9d78df4ee2
Swistaak <sswistak123@gmail.com>
2017-11-24 22:59:45 +0100

    GoogleMap + heatmap implemention

--- app/src/main/java/com/example/c/heatmap/MainActivity.java
+++ app/src/main/java/com/example/c/heatmap/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.c.heatmap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.heatmaps.WeightedLatLng;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.function.DoubleToLongFunction;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<LatLng> data = new ArrayList<LatLng>();
+    ArrayList<Float> intensity = new ArrayList<Float>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        data.add(new LatLng(30,40));
+        intensity.add(new Float(0.5f));
         setContentView(R.layout.activity_main);
-        //Ten komentarz mieć winieneś
-        //Ten komentarz mieć winieneś2
+        Button btn = (Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),MapsActivity.class);
+                i.putParcelableArrayListExtra("latLngData",data);
+                i.putExtra("intensityData",intensity);
+                startActivity(i);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/c/heatmap/MapsActivity.java
@@ -0,0 +1,94 @@
+package com.example.c.heatmap;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+import com.google.maps.android.heatmaps.WeightedLatLng;
+
+import java.io.Console;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    HeatmapTileProvider mProvider;
+    TileOverlay mOverlay;
+    ArrayList<WeightedLatLng> dataPoints;
+    ArrayList<LatLng> latLngData;
+    ArrayList<Float> intensityData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //create data array list(for heatmap points)
+        dataPoints = new ArrayList<WeightedLatLng>();
+
+        latLngData = new ArrayList<>();
+        latLngData = getIntent().getParcelableArrayListExtra("latLngData");
+
+        intensityData = new ArrayList<Float>();
+        intensityData = (ArrayList<Float>)getIntent().getSerializableExtra("intensityData");
+
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-34,151), 12.0f));
+        createHeatMap();
+    }
+    public void zoomOnPoint(double latitude, double longitude, float zoom){
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitude)));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude),zoom));
+    }
+    public void addPointToData(double latitude, double longitude, double intensity){
+        dataPoints.add(new WeightedLatLng(new LatLng(latitude,longitude),intensity));
+    }
+    public void createHeatMap(){
+        for (int i=0; i<latLngData.size(); i++){
+           dataPoints.add(new WeightedLatLng(new LatLng(latLngData.get(i).latitude,latLngData.get(i).longitude), intensityData.get(i).floatValue()));
+        }
+       mProvider = new HeatmapTileProvider.Builder().weightedData(dataPoints).build();
+        // Add a tile overlay to the map, using the heat map tile provider.
+       mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+    public void removeHeatMap(){
+        mOverlay.remove();
+    }
+
+}
+
