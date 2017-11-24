acec6e0600e9030ea52f746eb2049eb110031ba2
phaun <aleks1005@gmail.com>
2017-11-24 02:42:16 +0100

    dsafsdfsdfgdf

--- app/src/main/java/com/example/c/heatmap/MainActivity.java
+++ app/src/main/java/com/example/c/heatmap/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Ten komentarz mieć winieneś
+        //Ten komentarz mieć winieneś
     }
 }
