10483580be903ca94a3b7616053c22ed8b4fb0cb
Adramos <adramos@poczta.onet.pl>
2017-11-24 22:29:08 +0100

    My job here is fucked up v0.2.1 Takie tam: zabawa z wielkością czcionki Spinnera -dodano komentarze

--- app/src/main/java/com/example/c/heatmap/MainActivity.java
+++ app/src/main/java/com/example/c/heatmap/MainActivity.java
@@ -27,17 +27,18 @@ public class MainActivity extends AppCompatActivity {
         //vektor elementów
         final Vector<String> dostepneSieciVector;
         dostepneSieciVector = new Vector<String>();
-        dostepneSieciVector.add("<puste>");
+        dostepneSieciVector.add("<puste>");     //dodajemy opcje "puste". Zapytacie po co? Bo jak nie ma żaanego elementu przy tworzeniu to nie
+                                                //można wybrać żadnej opcji (wyświetla się, ale się nie wybiera)
 
         //spiner
-        //ArrayAdapter<String> dostepneSieciAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dostepneSieciVector);
-        ArrayAdapter<String> dostepneSieciAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout,dostepneSieciVector);
+        //ArrayAdapter<String> dostepneSieciAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dostepneSieciVector); //stare śmieci
+        ArrayAdapter<String> dostepneSieciAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout,dostepneSieciVector);    //dzięki temu możemy modyfikować wygląd czcionki (plik: spinner_layout.xml z folderu layout)
         final Spinner dostepneSieciSpinner = (Spinner) findViewById(R.id.listaDostepnychSieci);
         dostepneSieciSpinner.setAdapter(dostepneSieciAdapter);
 
-        dostepneSieciSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        dostepneSieciSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {           //co się stanie gdy wybierzemy...
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int numer, long l) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int numer, long l) {          //...element z listy
 
 
                 if(numer != 0) {        //po prostu by nie wyrzucało informacji na początku aplikacji o "wybraniu" jakiejś opcji
@@ -53,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            public void onNothingSelected(AdapterView<?> adapterView) {                                     //...nic (nie wiem jak to zrobić, ale podobno jest i taka opcja)
                 Toast.makeText(getApplicationContext(), "Nie wybrano niczego.", Toast.LENGTH_SHORT).show();
             }
         });
@@ -63,17 +64,17 @@ public class MainActivity extends AppCompatActivity {
         sprawdzSieci.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View arg0){
+            public void onClick(View arg0){                 //co się stanie gdy naciśniemy przycisk
                 Toast.makeText(getApplicationContext(), "Wyszukuję dostepnych sieci...", Toast.LENGTH_SHORT).show();
                 //W tym miejscu otrzymujemy listę/tablicę/chuj-wie-co zawierające nazwy dostępnych sieci.
                 int iloscSieci = 5;
-                if(dostepneSieciVector.size() > 1) {
-                    for (int i = dostepneSieciVector.size(); i > 1; i--) {
-                        dostepneSieciVector.remove(i-1);
+                if(dostepneSieciVector.size() > 1) {                        //jeśli w vectorze istnieją już jakies elementy (naciskamy po raz drugi+ przycisk)
+                    for (int i = dostepneSieciVector.size(); i > 1; i--) {  //to trzeba "odświeżyć" listę dostepnych sieci -> usunąć poprzednie i dodać wszystkie raz jeszcze.
+                        dostepneSieciVector.remove(i-1);              //ALE pozostawiamy element "zerowy", aby była opcja "puste" -> "nie dokonaj wyboru"
                     }
                 }
-                for(int i = 0; i < iloscSieci; i++){
-                    dostepneSieciVector.add("Opcja numer: " + i);
+                for(int i = 0; i < iloscSieci; i++){                        //dodajemy spis wszystkich wykrytych sieci wi-fi (numery elementów w vectorze: 1-(n+1),
+                    dostepneSieciVector.add("Opcja numer: " + i);           //gdzie "n" to liczba elementów (element zerowy to element "<puste>"
                 }
             }
 
